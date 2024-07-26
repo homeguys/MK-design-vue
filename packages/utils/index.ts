a9b92eb331ba8c7e93320d700fa97f27c53f7e42
wanglei <918061733@qq.com>
2024-07-26 13:45:44 +0800

    feat(utils): 增加formatLargeNum方法处理超长数值

--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -1,2 +1,11 @@
-export { default as withInstall } from './withInstall'
-export { default as makeInstaller } from './makeInstaller'
+/*
+ * @Author: wanglei
+ * @Date: 2024-07-25 14:08:02
+ * @LastEditors: wanglei
+ * @LastEditTime: 2024-07-26 13:44:55
+ * @FilePath: \packages\utils\index.ts
+ * @Descripttion:
+ */
+export { default as withInstall } from './withInstall';
+export { default as makeInstaller } from './makeInstaller';
+export { default as formatLargeNum } from './formatLargeNum';
